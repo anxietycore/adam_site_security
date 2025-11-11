3fcb9ced5f6ec8278c200c8513d5402acf1362e5
anxietycore <anxiety.merzost@gmail.com>
2025-11-11 13:51:44 +0300

    Delete screenCurvature_gl.js

--- screenCurvature_gl.js
+++
@@ -1,7 +0,0 @@
-// screenCurvature.js
-// Заглушка. Если ты раньше не использовал этот файл - он может быть пустым.
-// Нужен чтобы убрать 404 и не ломать порядок скриптов.
-(function(){
-  // noop - intentionally empty
-  window.__ScreenCurvature = window.__ScreenCurvature || { enabled: false };
-})();
