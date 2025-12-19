5c489836a42baf662f5b7b32b6d627f1a60a850d
anxietycore <anxiety.merzost@gmail.com>
2025-12-19 23:34:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// script.js — только логика переходов для index.html
-
-document.addEventListener('DOMContentLoaded', () => {
-    let visits = parseInt(localStorage.getItem('adam_visits')) || 0;
-    localStorage.setItem('adam_visits', ++visits);
-});
-
-// Эти функции вызываются из index_canvas.js
-window.startBootSequence = function() {
-    if (window.__startBoot) window.__startBoot();
-};
-
-window.login = function() {
-    if (window.__login) window.__login();
-};
+document.addEventListener("DOMContentLoaded",(()=>{let t=parseInt(localStorage.getItem("adam_visits"))||0;localStorage.setItem("adam_visits",++t)})),window.startBootSequence=function(){window.__startBoot&&window.__startBoot()},window.login=function(){window.__login&&window.__login()};
